fe85ebfdc456c23d5b2debae5099dbaa869686f5
YMolodyko <VTB4081847@region.vtb.ru>
2024-12-24 16:06:33 +0300

    fix mapstruct + lombok

--- src/main/java/com/javacademy/new_york_times/dto/NewsDto.java
+++ src/main/java/com/javacademy/new_york_times/dto/NewsDto.java
@@ -1,4 +1,11 @@
 package com.javacademy.new_york_times.dto;
 
+import lombok.Data;
+
+@Data
 public class NewsDto {
+    private Integer number;
+    private String title;
+    private String text;
+    private String author;
 }
